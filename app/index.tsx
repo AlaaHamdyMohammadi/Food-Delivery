3d386801c7a53f4992ce04c201cc9aee36503ac4
AlaaHamdy <alaahamdy2197@gmail.com>
2025-09-04 20:32:04 +0300

    feat: ✨ Implement home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,37 +1,75 @@
-import { offers } from "@/constants";
-import { FlatList, Image, Pressable, Text, View } from "react-native";
+import { images, offers } from "@/constants";
+import { FlatList, Image, Pressable, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import cn from "clsx";
+import CartButton from "@/components/CartButton";
 
 export default function Index() {
   return (
-    <SafeAreaView>
-      <FlatList data={offers} renderItem={({item, index}) => {
-        const isEven = index % 2 === 0;
-        return (
-          <View>
-            <Pressable
-              className={cn("offer-card", isEven && "flex-row-reverse")}
-              style={{ backgroundColor: item.color }}
-            >
-              {({ pressed }) => (
-                <>
-                  <View className={`h-full w-1/2`}>
-                    <Image
-                      source={item.image}
-                      className="size-full"
-                      resizeMode="contain"
-                    />
-                  </View>
-                  <View className={`offer-card__info`}>
-                    <Text>{item.title}</Text>
-                  </View>
-                </>
-              )}
-            </Pressable>
+    <SafeAreaView className="flex-1 bg-white">
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0;
+          return (
+            <View>
+              <Pressable
+                className={cn(
+                  "offer-card",
+                  isEven ? "flex-row-reverse" : "flex-row"
+                )}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: "#fffff2" }}
+              >
+                {({ pressed }) => (
+                  <>
+                    <View className={`h-full w-1/2`}>
+                      <Image
+                        source={item.image}
+                        className="size-full"
+                        resizeMode="contain"
+                      />
+                    </View>
+                    <View
+                      className={cn(
+                        `offer-card__info`,
+                        isEven ? "pl-10" : "pr-10"
+                      )}
+                    >
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className="size-10"
+                        resizeMode="contain"
+                        tintColor="#fff"
+                      />
+                    </View>
+                  </>
+                )}
+              </Pressable>
+            </View>
+          );
+        }}
+        contentContainerClassName="pb-28 p-5"
+        ListHeaderComponent={() => (
+          <View className="flex-between flex-row items-center w-full my-5">
+            <View className="flex justify-start gap-0.5">
+              <Text className="small-bold text-primary">DELIVER TO</Text>
+              <TouchableOpacity className="flex flex-row items-center gap-1">
+                <Text className="paragraph-bold text-dark-100">Egypt</Text>
+                <Image
+                  source={images.arrowDown}
+                  className="size-3"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+            <CartButton/>
           </View>
-        );
-      }} />
+        )}
+      />
     </SafeAreaView>
   );
 }
